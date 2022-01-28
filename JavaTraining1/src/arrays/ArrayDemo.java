e21a44be2b1361684c6a37fe60730108db52870f
appzgames <73015785+appzgames@users.noreply.github.com>
2022-01-28 15:39:56 +0530

    Add files via upload

---
+++ JavaTraining1/src/arrays/ArrayDemo.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		
+		int[] array1 = {1,2,3,4,5};
+		
+		//or
+		
+		int[] arr = new int [5]; //array declaration
+		arr[0] = 11;
+		arr[1] = 22;
+		arr[2] = 33;
+		arr[3] = 44;
+		arr[4] = 55;
+		
+
+		//System.out.println(arr[4]);	
+		
+		String [] countrynames = {"India", "USA", "China"};
+		
+		System.out.println(countrynames.length);
+		
+		
+	}
+
+}
